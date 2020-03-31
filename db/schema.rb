09e26baaf91ee2ab4c6dbd1f306c0f74bcc5b964
Quentin Messager <qmessager@gmail.com>
2020-03-31 15:18:48 +0200

    CRUD done ugly tho

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_31_093536) do
-
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
